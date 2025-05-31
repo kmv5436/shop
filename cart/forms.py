0b638535dc6aee18d605fd9fefa12665e4dfe593
Mkrtych <mirumir5436@yandex.ru>
2025-06-01 00:14:21 +0300

    Step 433

--- cart/forms.py
+++ cart/forms.py
@@ -1,20 +1,25 @@
 from django import forms
-from django.utils.translation import gettext_lazy as _  # Импорт функции для перевода
+from django.utils.translation import gettext_lazy as _
 
-
-# Русскоязычные варианты количества
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
-
 
 class CartAddProductForm(forms.Form):
     quantity = forms.TypedChoiceField(
         choices=PRODUCT_QUANTITY_CHOICES,
         coerce=int,
-        label=_('Количество'),  # Русская метка для поля
+        label=_('Количество'),
+        widget=forms.Select(attrs={
+            'class': 'form-control quantity-select',
+            'style': 'width: 80px; display: inline-block;'
+        })
     )
     override = forms.BooleanField(
         required=False,
         initial=False,
-        widget=forms.HiddenInput,
-        label=_('Перезаписать'),  # Русская метка для поля (хотя оно скрытое)
+        widget=forms.HiddenInput
     )
+
+    def __init__(self, *args, **kwargs):
+        initial_quantity = kwargs.pop('initial_quantity', 1)
+        super().__init__(*args, **kwargs)
+        self.fields['quantity'].initial = initial_quantity
