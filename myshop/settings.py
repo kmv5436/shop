961d49012875be931fc891bcbd5424201d01e7a1
Mkrtych <mirumir5436@yandex.ru>
2025-06-27 00:20:38 +0300

    Step sait1

--- myshop/settings.py
+++ myshop/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 from django.utils.translation import gettext_lazy as _
 
@@ -21,13 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^*@dt0nqvfc#h+qmjg=6it7qqk)61u1n^tio79d$+e3dv%!y*y'
-
+#SECRET_KEY = 'django-insecure-^*@dt0nqvfc#h+qmjg=6it7qqk)61u1n^tio79d$+e3dv%!y*y'
+SECRET_KEY = get_random_secret_key()
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 # Application definition
 
@@ -126,6 +130,7 @@ LANGUAGES = [
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
